d68590313f3d45fa9aa018d917d3e5928ded2b88
Jorge Paul <jserranopaul@gmail.com>
2022-03-29 17:16:40 +0200

    PUT DEL

--- controllers/dogs.js
+++ controllers/dogs.js
@@ -5,7 +5,7 @@ export const getAllDogs = async (req, res) => {
     const { rows: dogs } = await pool.query('SELECT * FROM dogs;');
     res.json(dogs);
   } catch (error) {
-    res.status(500).json(error);
+    res.status(500).json({ error: error.message });
   }
 };
 
@@ -39,12 +39,51 @@ export const getSingleDog = async (req, res) => {
       rows: [dog]
     } = await pool.query('SELECT * FROM dogs WHERE id = $1', [id]);
     if (!rowCount) return res.status(404).json({ error: `Doggo with id of ${id} not found` });
+
     res.json(dog);
   } catch (error) {
-    res.status(500).json(error);
+    res.status(500).json({ error: error.message });
   }
 };
 
-export const updateDog = (req, res) => res.send('PUT single dog');
+export const updateDog = async (req, res) => {
+  try {
+    const {
+      params: { id },
+      body: { name, breed, gender, color, alphanum, kid_friendly, haftpflichtversicherung }
+    } = req;
+    const { rowCount: found } = await pool.query('SELECT * FROM dogs WHERE id = $1', [id]);
+    if (!found) {
+      throw new Error(`The doggo you are trying to update doesn't exist`);
+    }
+    if (!name || !breed || !gender || !color || !alphanum) {
+      throw new Error('Invalid body');
+    }
+    const {
+      rows: [dog]
+    } = await pool.query(
+      'UPDATE dogs SET name = $2, breed = $3, gender = $4, color = $5 , alphanum = $6, kid_friendly = $7, haftpflichtversicherung = $8 WHERE id = $1 RETURNING *',
+      [id, name, breed, gender, color, alphanum, kid_friendly, haftpflichtversicherung]
+    );
+    res.json(dog);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
-export const deleteDog = (req, res) => res.send('DELETE single dog');
+export const deleteDog = async (req, res) => {
+  try {
+    const {
+      params: { id }
+    } = req;
+    const { rowCount } = await pool.query('DELETE FROM dogs WHERE id = $1 RETURNING *', [id]);
+    if (rowCount) {
+      return res.json({ msg: `Doggo with id of ${id} was deleted` });
+    } else {
+      throw new Error(`Doggo with id of ${id} doesn't even exist`);
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+    x;
+  }
+};
