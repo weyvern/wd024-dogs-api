a8a0622fc3fc4347a3bc5fb02832d9fd775ce03d
Jorge Paul <jserranopaul@gmail.com>
2022-03-29 13:12:31 +0200

    routes, controllers, pg, crud

--- controllers/dogs.js
+++ controllers/dogs.js
@@ -1,25 +1,48 @@
-// DATABASE
-const dogs = [
-  { id: 1, breed: 'Corgi', size: 'smol' },
-  { id: 2, breed: 'Chihuahua', size: 'smol' }
-];
+import pool from '../db/pg.js';
 
-export const getAllDogs = (req, res) => res.json(dogs);
-
-export const createDog = (req, res) => {
-  const { body } = req;
-  const newDog = { id: dogs.length + 1, ...body };
-  dogs.push(newDog);
-  res.status(201).json(newDog);
+export const getAllDogs = async (req, res) => {
+  try {
+    const { rows: dogs } = await pool.query('SELECT * FROM dogs;');
+    res.json(dogs);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 };
 
-export const getSingleDog = (req, res) => {
-  const {
-    params: { id }
-  } = req;
-  const found = dogs.find(dog => dog.id === parseInt(id));
-  if (!found) return res.status(404).json({ msg: 'No doggo for you' });
-  res.json(found);
+export const createDog = async (req, res) => {
+  try {
+    const {
+      body: { name, breed, gender, color, alphanum, kid_friendly, haftpflichtversicherung }
+    } = req;
+    if (!name || !breed || !gender || !color || !alphanum) {
+      throw new Error('Invalid body');
+    }
+    const query =
+      'INSERT INTO dogs (name, breed, gender, color, alphanum, kid_friendly, haftpflichtversicherung) VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING *';
+    const values = [name, breed, gender, color, alphanum, kid_friendly, haftpflichtversicherung];
+    const {
+      rows: [newDog]
+    } = await pool.query(query, values);
+    res.status(201).json(newDog);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const getSingleDog = async (req, res) => {
+  try {
+    const {
+      params: { id }
+    } = req;
+    const {
+      rowCount,
+      rows: [dog]
+    } = await pool.query('SELECT * FROM dogs WHERE id = $1', [id]);
+    if (!rowCount) return res.status(404).json({ error: `Doggo with id of ${id} not found` });
+    res.json(dog);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 };
 
 export const updateDog = (req, res) => res.send('PUT single dog');
